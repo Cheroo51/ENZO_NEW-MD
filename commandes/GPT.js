933234e3f6d3cf9084c79be0ae58381c15b3e436
ENZO <brendacheroo428@gmail.com>
2024-09-28 07:52:13 +0300

    Update GPT.js

--- commandes/GPT.js
+++ commandes/GPT.js
@@ -8,7 +8,7 @@ Created By POPKID_TECH
 Don't claim, okey 
 */
 
-zokou({nomCom:"gpt4",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"gpt4",reaction:"☀️",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
